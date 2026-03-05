130f2d7a033f09a6cb81696f498a5eced45bdd23
cristian Arturo Pardo <cristianpardorivero@gmail.com>
2026-03-05 01:06:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2230611052-d47e4",
+  "appId": "1:626692625024:web:adabf43d28fd4f79124c65",
+  "apiKey": "AIzaSyDU5UGfZ2FK_-aCSOsijoVKsLObmXbajAo",
+  "authDomain": "studio-2230611052-d47e4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "626692625024"
+};
