db2000ab700ec5db914305744dfc6bab73b29e2f
cristian Arturo Pardo <cristianpardorivero@gmail.com>
2026-03-05 18:32:41 +0000

    mira creo q aca tengo un error

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "appId": "1:626692625024:web:adabf43d28fd4f79124c65",
   "apiKey": "AIzaSyDU5UGfZ2FK_-aCSOsijoVKsLObmXbajAo",
   "authDomain": "studio-2230611052-d47e4.firebaseapp.com",
-  "storageBucket": "acreditacion.appspot.com",
+  "storageBucket": "studio-2230611052-d47e4.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "626692625024"
 };
