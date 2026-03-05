4bd8d1446fc897937ad02f01130e553387102dd6
cristian Arturo Pardo <cristianpardorivero@gmail.com>
2026-03-05 01:10:19 +0000

    Try fixing this error: `Console Error: [2026-03-05T01:08:25.396Z] @fireb

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp, getApps, FirebaseApp } from "firebase/app";
-import { getAuth, connectAuthEmulator } from "firebase/auth";
-import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
-import { getStorage, connectStorageEmulator } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 import { firebaseConfig } from "@/firebase/config";
 
 
@@ -15,15 +15,3 @@ if (!getApps().length) {
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
-
-// Connect to emulators if running in development
-if (process.env.NODE_ENV === 'development') {
-    try {
-        connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
-        connectFirestoreEmulator(db, "127.0.0.1", 8080);
-        connectStorageEmulator(storage, "127.0.0.1", 9199);
-        console.log("Connected to Firebase Emulators");
-    } catch(e) {
-        console.warn("Could not connect to emulators, this is expected in production. Error: ", e);
-    }
-}
