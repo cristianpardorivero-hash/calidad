ec9a1a401f2566e2d05c486f06df90d1e1ef0177
cristian Arturo Pardo <cristianpardorivero@gmail.com>
2026-03-06 18:27:43 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'pd

--- src/components/documents/document-preview-modal.tsx
+++ src/components/documents/document-preview-modal.tsx
@@ -21,7 +21,7 @@ import 'react-pdf/dist/esm/Page/TextLayer.css';
 
 // Use the recommended way to set the worker source for Next.js
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.js',
+  'pdfjs-dist/build/pdf.worker.min.mjs',
   import.meta.url,
 ).toString();
 
